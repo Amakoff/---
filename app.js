52e8a20d89cfced1c9d3fc6757319fe4de4495db
Никита <109104779+Amakoff@users.noreply.github.com>
2023-10-26 20:33:38 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,34 @@
+const board = document.querySelector('#board')
+
+const colors = ['#e74c3c', '#8e44ad', '#3498db', '#e67e22', '#2ecc71', '#CD5C5C', '#ADFF2F', '#3CB371', '#FFA500', '#FFEFD5', '#4169E1']
+
+const SQUARES_NUMBER = 1120
+
+for (let i = 0; i < SQUARES_NUMBER; i++) {
+    const square = document.createElement('div')
+    square.classList.add('square')
+
+    square.addEventListener('mouseover', () => 
+    setColor(square))
+
+    square.addEventListener('mouseleave', () => 
+    removeColor(square))
+
+    board.append(square)
+}
+
+function setColor(element) {
+    const color = getRandomColor()
+    element.style.backgroundColor = color
+    element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
+}
+
+function removeColor(element) {
+    element.style.backgroundColor = '#1d1d1d'
+    element.style.boxShadow = `0 0 2px #000`
+}
+
+function getRandomColor() {
+    const index = Math.floor(Math.random() * colors.length)
+    return colors[index]
+}
